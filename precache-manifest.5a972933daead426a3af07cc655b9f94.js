df0d5d88462c0df50d5e39837c4a2489525855e3
alwaysbaguette <alwaysbaguette@gmail.com>
2019-04-02 23:06:27 +0900

    Updates

---
+++ precache-manifest.5a972933daead426a3af07cc655b9f94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adb43b6265d846b49015",
+    "url": "/movie_app/static/css/main.59b1b826.chunk.css"
+  },
+  {
+    "revision": "adb43b6265d846b49015",
+    "url": "/movie_app/static/js/main.af02b733.chunk.js"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "94b05b5e27f866f87c51",
+    "url": "/movie_app/static/js/2.acedb997.chunk.js"
+  },
+  {
+    "revision": "c09324b707ced9ef791670781e065d6f",
+    "url": "/movie_app/index.html"
+  }
+];
